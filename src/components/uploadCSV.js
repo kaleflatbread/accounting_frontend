feb309e4189ce51b787e87dc4f5b40c9072ac4ef
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-08-10 11:38:19 -0400

    csv data parsed into object

--- src/components/uploadCSV.js
+++ src/components/uploadCSV.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import ReactFileReader from 'react-file-reader';
+import Button from '@material-ui/core/Button';
 
 
 class UploadCSV extends Component {
   handleFiles = files => {
     var reader = new FileReader();
     reader.onload = function(e) {
-    // Use reader.result
-    console.log(reader.result)
+    const data = reader.result
+    var lines = data.split('\n');
+    const headers = lines[0].split(',')
+    const upload = []
+      for (var i = 1; i < lines.length; i++){
+        var transaction = lines[i].split(',');
+        var tmpTransaction = {}
+        tmpTransaction[headers[0]] = transaction[0];
+        tmpTransaction[headers[1]] = transaction[1]
+        tmpTransaction[headers[2]] = transaction[2]
+        upload.push(tmpTransaction)
+      }
     }
   reader.readAsText(files[0]);
 }
-
-
-
-
 
   render() {
     return (
@@ -25,7 +32,7 @@ class UploadCSV extends Component {
         <br/>
       INVENTORY PAGE!!
       <ReactFileReader handleFiles={this.handleFiles} fileTypes={'.csv'}>
-          <button className='btn'>Upload</button>
+          <Button>Browse Files</Button>
       </ReactFileReader>
       </div>
     );
