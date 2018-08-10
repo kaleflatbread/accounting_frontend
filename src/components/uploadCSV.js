f705077b507f1d8c6d6df500dd1c5b206d2eb0db
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-08-10 11:05:14 -0400

    FILE UPLOADER

--- src/components/uploadCSV.js
+++ src/components/uploadCSV.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+import ReactFileReader from 'react-file-reader';
 
 
 class UploadCSV extends Component {
+  handleFiles = files => {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+    // Use reader.result
+    console.log(reader.result)
+    }
+  reader.readAsText(files[0]);
+}
+
+
+
+
+
   render() {
     return (
       <div>
@@ -10,6 +24,9 @@ class UploadCSV extends Component {
         <br/>
         <br/>
       INVENTORY PAGE!!
+      <ReactFileReader handleFiles={this.handleFiles} fileTypes={'.csv'}>
+          <button className='btn'>Upload</button>
+      </ReactFileReader>
       </div>
     );
   }
