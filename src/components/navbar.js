70f1658682e3184d90ce7750ce07a262182f3bac
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-08-22 11:07:37 -0400

    conditional rendering of login logout

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink} from 'mdbreact';
+import { connect } from 'react-redux';
+
 
 class NavbarFeatures extends React.Component {
   constructor(props) {
@@ -16,7 +18,7 @@ class NavbarFeatures extends React.Component {
 
 
 
-  render() {
+  render() { if (this.props.userId) {
     return (
         <Navbar className="navbar" dark expand="md" scrolling>
           <NavbarBrand href="/">
@@ -36,16 +38,34 @@ class NavbarFeatures extends React.Component {
               </NavItem>
             </NavbarNav>
             <NavbarNav right>
-            { /* <NavItem>
-                <form className="form-inline md-form mt-0">
-                  <input className="form-control mr-sm-2 mb-0 text-white" type="text" placeholder="Search" aria-label="Search"/>
-                </form>
-              </NavItem>*/ }
-              <NavItem>
-                  <NavLink to="/login">Login</NavLink>
-              </NavItem>
               <NavItem>
                   <NavLink onClick={this.logout} to="/login">Logout</NavLink>
+              </NavItem>
+            </NavbarNav>
+          </Collapse>
+        </Navbar>
+    )} else {
+      return (
+        <Navbar className="navbar" dark expand="md" scrolling>
+          <NavbarBrand href="/">
+            <strong>ABY</strong>
+          </NavbarBrand>
+            { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
+          <Collapse isOpen = { this.state.collapse } navbar>
+            <NavbarNav left>
+              <NavItem>
+                <NavLink to="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/inventory/new">Upload CSV</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/expense/new">Submit Expenses</NavLink>
+              </NavItem>
+            </NavbarNav>
+            <NavbarNav right>
+              <NavItem>
+                  <NavLink to="/login">Login</NavLink>
               </NavItem>
               <NavItem>
                   <NavLink to="/user/new">Register</NavLink>
@@ -53,7 +73,15 @@ class NavbarFeatures extends React.Component {
             </NavbarNav>
           </Collapse>
         </Navbar>
-    );
+      )
+    }
   }
 }
-export default NavbarFeatures
+
+function mapStateToProps(state) {
+  return {
+    userId: state.userId,
+  }
+}
+
+export default connect(mapStateToProps)(NavbarFeatures)
